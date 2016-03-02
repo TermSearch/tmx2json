803c79494b653c5b9d25f010373ccd5c5bbb7c9d
Koen van Gilst <koen.vangilst@gmail.com>
2016-03-02 15:52:40 +0100

    adds basic loggin of results

--- tmx-parse.js
+++ tmx-parse.js
@@ -1,20 +1,45 @@
-var cheerio = require('cheerio');
-var iconv = require('iconv-lite');
+'use strict'
 
-var fs = require('fs');
-var readableStream = fs.createReadStream('testing-data/tm2010-first-5K-lines.tmx');
-var data = '';
-var str = '';
-var utf8str = '';
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+const fs = require('fs');
+
+// Command line options
+const inputFile = process.argv[2];
+const outputFile = process.argv[3] || process.argv[2] + '.json';
+
+const readableStream = fs.createReadStream(inputFile);
 
 readableStream.on('data', function(chunk) {
-    str = iconv.decode(chunk, 'utf-16le');
-    data += str;
-    var $ = cheerio.load(chunk, { xmlMode: true });
-    var tu = $('tu');
-    console.log(tu.text());
+
+    // decode utf-16le in tmx file
+    let str = iconv.decode(chunk, 'utf-16le');
+    let $ = cheerio.load(str, { xmlMode: true });
+    let termUnits = $('tu');
+
+    termUnits.each(function(i, termUnit) {
+      let docId = $(this).find('prop').text();
+      console.log("ID: " + docId);
+      let terms = $(this).find('tuv');
+      terms.each(function(i, term) {
+        let lang = $(this).attr('xml:lang');
+        let text = $(this).find('seg').text();
+        console.log(lang + ": " + text);
+      })
+    })
+
+    // let lang = termUnit.
+    // // let lang = termUnit.find('tuv').attr('xml:lang');
+    // // let termStr = termUnit.find('tuv').find('seg').text();
+    // // let output = "Language: "+termUnit[0].find('tuv');
+    // let t = cheerio.load(termUnit[1], { xmlMode: true });
+    // let output = t('tuv').attr('xml:lang');
+    // console.log(output);
 });
 
 readableStream.on('end', function() {
-    // console.log(data);
+
 });
+
+
+// url: http://eur-lex.europa.eu/legal-content/EN-DE-NL/TXT/?uri=CELEX:32004R1788&from=EN
